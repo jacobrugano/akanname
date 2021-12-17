274e76d13d71eba8d6dd1b85644f3cd180884c21
jacobrugano <jacob.rugano@student.moringaschool.com>
2021-12-17 12:52:54 +0300

    Added the onclick funtion on submi button

--- js/script.js
+++ js/script.js
@@ -1,89 +1,93 @@
+// Declaring variables from the userinput and assigning values to them
 function findName(){
   var date = document.getElementById('date').value;
   var gender = document.getElementById('gender').value;
   var month = document.getElementById('month').value;
   var year = document.getElementById('year').value;
 
+//Declaring variables from the formulae.
+  var cc = parseInt(c);
+  var c = year.slice(0, 2);
+  var yy = parseInt(y);
+  var y = year.slice(2, 4);
+  var dd = parseInt(date);
+  var mm = parseInt(month);
+  var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 //d representing the day of the week.
+
 if (date <=0 || date > 31) {
-  alert("Please enter a valid date");
+  alert("Enter a valid date");
   return;
 }
 else if (month <= 0 || month >12) {
-  alert("Please enter a valid month")
+  alert("Enter a valid month")
   return;
 };
-var dd = parseInt(date);
-var y = year.slice(2, 4);
-var c = year.slice(0, 2);
-var yy = parseInt(y);
-var mm = parseInt(month);
-var cc = parseInt(c);
-var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
-if (gender === "Female") {
-  function Female(){
-    var d = d.toFixed();
-    var Female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    switch (d) {
-
-      case "2":
-        alert("Your Akan name is:" + female(0));
-        break;
-
-      case "3":
-      alert("Your Akan name is: ""+ female(1));
-      break;
-
-      case "4";
-      alert("Your Akan name is:"+ female(2));
-      break;
-
-      case "5";
-      alert("Your Akan name is: " + female(3));
-      break;
-
-      case "6";
-      alert("Your Akan name is: " + female(4));
-      break;
-
-    }
-  }
-  Female();
-}
+//Declaring the Akannames
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 if (gender === "male") {
   function male(){
     var d = d.toFixed();
-    var Female = ["Kwasi","Kwadwo","Kwabena","Kwahu","Yaw","Kofi","Kwame"];
     switch (d) {
 
-      case "2":
+      case "b":
         alert("Your Akan name is:" + male(0));
         break;
-
-      case "3":
+ // block of code to be executed if the condition is true
+      case "c":
       alert("Your Akan name is: ""+ male(1));
       break;
-
-      case "4";
+ // block of code to be executed if the condition is true
+      case "d";
       alert("Your Akan name is:"+ male(2));
       break;
-
-      case "5";
+ // block of code to be executed if the condition is true
+      case "e";
       alert("Your Akan name is: " + male(3));
       break;
-
-      case "6";
+ // block of code to be executed if the condition is true
+      case "f";
       alert("Your Akan name is: " + male(4));
       break;
-
+ // block of code to be executed if the condition is true
     }
   }
   male();
 }
 
+if (gender === "Female") {
+  function Female(){
+    var d = d.toFixed();
+    switch (d) {
+      case "b":
+        alert("Your Akan name is:" + female(0));
+        break;
+ // block of code to be executed if the condition is true
+      case "c":
+      alert("Your Akan name is: ""+ female(1));
+      break;
+ // block of code to be executed if the condition is true
+      case "d";
+      alert("Your Akan name is:"+ female(2));
+      break;
+ // block of code to be executed if the condition is true
+      case "e";
+      alert("Your Akan name is: " + female(3));
+      break;
+ // block of code to be executed if the condition is true
+      case "f";
+      alert("Your Akan name is: " + female(4));
+      break;
+ // block of code to be executed if the condition is true
+    }
+  }
+  Female();
+}
+
 if (gender === "") {
-  alert("please choose a gender");
+  alert("Ensure you choose one gender");
 }
 
 
